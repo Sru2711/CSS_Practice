aac7b11fff104493ee4c5017d5d0296713a22b33
Sru2711 <srushtimalagi@gmail.com>
2024-09-12 11:48:59 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import logo from './logo.svg';
-import './App.scss';
-import { useRoutes } from 'react-router-dom';
-import { Button } from 'reactstrap';
-import drawer from "../src/Assets/drawer.png"
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-function App() {
-  const [toggle, setToggle]=useState(false)
+import './App.css';
 
+function App() {
   return (
     <div className="App">
-      
-      <div className={toggle ? "navbar-divopen" : "navbar-divclose"}>
-      <div className={toggle ? "drawer-divopen" : "drawer-divclose"}>
-      <img src={drawer} className="drawer-img" onClick={()=> {setToggle(!toggle)}}></img>
-      </div>
-      <Navbar></Navbar>
-      </div>
-       <div className={toggle ? "content-divopen" : "content-divclose"}>
-        hello
-       </div>
-      
+      Hello World!!!!
     </div>
   );
 }
